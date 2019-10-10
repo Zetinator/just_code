75481c357b651d5b067873aef8351c43eaa3f1a4
Zetinator <erick_zetina@hotmail.com>
2019-10-11 01:53:14 +0200

    fill_matrick now with propper backtracking

--- fill_matrix.py
+++ fill_matrix.py
@@ -3,32 +3,33 @@ Input: n = 2
 Output: null
 Explanation: We need to fill [1, 2, 3, 4] into a 2x2 matrix, which is not possible so return null.
 """
-def check_magic(x: 'flat matrix', n: 'NxN'):
-    print(f'CHECKING: {x}', end='\r')
-    magic = sum(x[:n])
-    diagonal = 0
-    a_diagonal = 0
-    for i in range(n):
-        if sum(x[i*n:(i+1)*n]) != magic: return False
-        diagonal += x[i*n+i]
-        a_diagonal += x[i*n+(n-(i+1))]
-        sum_col = 0
-        for j in range(n):
-            sum_col += x[j*n+i]
-        if sum_col != magic: return False
-    if not diagonal == magic or not a_diagonal == magic: return False
-    print('\n...SUCCESS')
-    return True
-
 def fill_matrix(n: 'matrix NxN dimensions') -> 'matrix filled':
-    numbers = [e for e in range(1, n**2+1)]
+    numbers = [e for e in range(1, n**2+1)]  # those are rockie numbers
     def go_deep(numbers, n, ans):
-        if not numbers:
-            if check_magic(ans, n):
-                return ans
-            else:
-                return None
-        # print(f'STATUS: numbers:{numbers}, ans:{ans}')
+        # reject
+        if len(ans) > n:  # row by row
+            temptation = sum(ans[:n])
+            for i in range(1,len(ans)//n):
+                if sum(ans[n*i:n*(i+1)]) != temptation: return None
+        if len(ans) > (n**2 - n):  # column by column
+            for i in range(n - len(numbers)):
+                sum_column = 0
+                for j in range(n):
+                    sum_column += ans[j*n + i]
+                if sum_column != temptation: return None
+        if len(ans) == n**2: # diagonal by diagonal
+            diagonal = 0
+            a_diagonal = 0
+            for i in range(n):
+                diagonal += ans[i*n+i]
+                a_diagonal += ans[i*n+(n-(i+1))]
+            if diagonal != temptation or a_diagonal != temptation: return None
+            print('...SUCCESS')
+
+        # long live the kings
+        if not numbers: return ans  # we are done
+        # search and destroy
+        print(f'STATUS: ans:{ans}')
         for i in range(len(numbers)):
             r = go_deep(numbers[:i]+numbers[i+1:], n, ans+[numbers[i]])
             if r: return r

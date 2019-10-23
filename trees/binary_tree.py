529ead4e1f778234aa358cb2b5b09d029ce7cb62
Zetinator <erick.zetina@audi.de>
2019-10-23 15:36:23 +0200

    binary search tree added...

--- trees/binary_tree.py
+++ trees/binary_tree.py
@@ -33,11 +33,15 @@ class bt():
 
     def search(self, v):
         def deep(current_node):
-            if not current_node: return False
-            if current_node.value == v: return True
-            return deep(current_node.left) or deep(current_node.right)
-        if not deep(self.root): raise ValueError(f'{v} is not in the tree')
-        return True
+            if not current_node: return None
+            if current_node.value == v: return current_node
+            if v < current_node.value:
+                return deep(current_node.left)
+            else:
+                return deep(current_node.right)
+        node = deep(self.root)
+        if not node: raise ValueError(f'{v} is not in the tree')
+        return node
 
     def insert(self, v):
         if not self.root:  # empty tree
@@ -57,65 +61,56 @@ class bt():
                 else: return deep(v, current_node.right)
         return deep(v, self.root)
 
-    def find_successor(self, node):
-        print('looking...')
+    def retrieve_successor(self, node):
+        # first some pruning
         if not node: return
-        def deep(current_node):
-            if not current_node.left: return current_node
-            return deep(current_node.left)
         if not node.right: return
-        return deep(node.right)
+        def deep(parent, current_node, righteous=1):  # comes from the right side of the parent
+            if not current_node.left:  # found it
+                if righteous:
+                    if current_node.right: parent.right = current_node.right
+                    if not current_node.right: parent.right = None
+                else:
+                    if current_node.right: parent.left = current_node.right
+                    if not current_node.right: parent.left = None
+                return current_node
+            return deep(current_node, current_node.left)
+        return deep(node, node.right, 1)
 
     def delete(self, v):
-        def deep(v, parent, current_node):
+        def deep(v, parent, current_node, righteous=1):
             if not current_node: return False
-            print(f'STATUS: searching for: {v}, current_node: {current_node.value}')
             if current_node.value == v:
                 # case: no children --> set None
                 if not current_node.left and not current_node.right:
-                    print('no children...')
-                    if not parent:
+                    if not parent:  # are you root?
                         self.root = None
                         return True
-                    if current_node.value > parent.value:
+                    if righteous:
                         parent.right = None
                     else:
                         parent.left = None
                     return True
                 # case: 2 children --> replace with successor
                 if current_node.left and current_node.right:
-                    print(f'two children... look for successor...')
-                    if not parent:
-                        tmp = self.find_successor(current_node)
-                        tmp.left, tmp.right = current_node.left, current_node.right
-                        self.delete(tmp.value)
-                        self.root = tmp
-                        return True
-                    if current_node.value > parent.value:
-                        tmp = self.find_successor(current_node)
-                        tmp.left, tmp.right = current_node.left, current_node.right
-                        self.delete(tmp.value)
-                        parent.right = tmp
-                    else:
-                        tmp = self.find_successor(current_node)
-                        tmp.left, tmp.right = current_node.left, current_node.right
-                        self.delete(tmp.value)
-                        parent.left = tmp
+                    tmp_node = self.retrieve_successor(current_node)
+                    current_node.value = tmp_node.value
                     return True
                 # case: one children --> bypass
                 if current_node.left or current_node.right:
-                    print(f'one children... bypassing...')
-                    if not parent:
+                    if not parent: # are you root?
                         self.root = current_node.left or current_node.right
                         return True
-                    if current_node.value > parent.value:
+                    if righteous:
                         parent.right = current_node.left or current_node.right
                     else:
                         parent.left = current_node.left or current_node.right
                     return True
             # sorry mate... keep looking
-            return deep(v, current_node, current_node.left) or \
-                   deep(v, current_node, current_node.right)
+            if v < current_node.value:
+                return deep(v, current_node, current_node.left, 0)
+            else:
+                return deep(v, current_node, current_node.right, 1)
         if not deep(v, None, self.root): raise KeyError(f'{v} is not in the tree, nothing to remove')
         return True
 

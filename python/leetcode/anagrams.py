da7232b1071e51a00f60aeb57734dc25709ad3af
Zetinator <erick_zetina@hotmail.com>
2019-12-08 01:22:14 -0600

    sherlock and the anagrams added

--- python/leetcode/anagrams.py
+++ python/leetcode/anagrams.py
@@ -1,19 +1,23 @@
+"""
+https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?h_r=internal-search
+"""
+
 def sherlockAndAnagrams(s):
     sets = []
-	s_counter = Counter()
+    s_counter = Counter()
     for i,_ in enumerate(s):
         for j in range(i, len(s)):
-			substring = s[i:j+1]
+            substring = s[i:j+1]
             if Counter(substring) in sets:
-				index = sets.index(Counter(substring))
-				s_counter[index] += 1
+                index = sets.index(Counter(substring))
+                s_counter[index] += 1
                 print(f's: {s[i:j+1]} is in')
             else:
-				print(f'adding {s[i:j+1]} to sets: {sets}')
+                print(f'adding {s[i:j+1]} to sets: {sets}')
                 sets.append(Counter(substring))
-	print(f's_counter: {s_counter}')
-	acc = 0
-	for e in s_counter.values():
-		acc += math.factorial(e+1) // (2*math.factorial(e-1))
+        print(f's_counter: {s_counter}')
+        acc = 0
+        for e in s_counter.values():
+            acc += math.factorial(e+1) // (2*math.factorial(e-1))
     return acc
 

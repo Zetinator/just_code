34e281cfdd6fee2036399ffc481e09082ca36ffc
Zetinator <erick_zetina@hotmail.com>
2019-12-12 22:10:31 -0600

    min_swaps_2 solved, but not in the limit time :/

--- python/leetcode/min_swaps_2.py
+++ python/leetcode/min_swaps_2.py
@@ -4,18 +4,22 @@ You are given an unordered array consisting of consecutive integers  [1, 2, 3, .
 """
 
 def minimumSwaps(x):
-    right_pos = {e: i for i, e in enumerate(sorted(x))}
+    sorted_x = sorted(x)
+    right_pos = {e: i for i, e in enumerate(sorted_x)}
     def entropy(x, right_pos):
-        return sum([abs(i-right_pos[e]) for i,e in enumerate(x)])
+        return [i-right_pos[e] for i,e in enumerate(x)]
     swaps = 0
-    while entropy(x, right_pos) != 0:
+    while sorted_x != x:
         swaps += 1
         minima = min([i for i in range(len(x))], key=lambda i: i-right_pos[x[i]])
         maxima = max([i for i in range(minima, len(x))], key=lambda i: i-right_pos[x[i]])
+        print(f'entropy current state: x: {x}, entropy: {entropy(x, right_pos)}')
+        print(f'min: {minima}, maxima: {maxima}')
         x[minima], x[maxima] = x[maxima], x[minima]
     return swaps
 
 test = [7, 1, 3, 2, 4, 5, 6]
+test = [4, 3, 1, 2]
 # test = [2, 1, 3, 7, 4, 5, 6]
 # test = [1, 2, 3, 7, 4, 5, 6]
 # test = [1, 2, 3, 4, 7, 5, 6]

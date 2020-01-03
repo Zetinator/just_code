05b190b132cf34c0112d20b410706f8e5a142fe3
Zetinator <erick_zetina@hotmail.com>
2020-01-02 22:35:19 -0600

    treaps working...

---
+++ python/data_structures/treap.py
@@ -0,0 +1,135 @@
+"""this is the implementation of a treap class
+https://en.wikipedia.org/wiki/Treap
+the ADT contains the following methods:
+    - insert
+    - search
+    - delete
+"""
+
+class Treap():
+    class Node():
+        """Node basic chainable storage unit
+        """
+        def __init__(self, value=None):
+            self.value = value
+            self.priority = uniform(0,1)
+            self.left = None
+            self.right = None
+
+        def __repr__(self):
+            return repr(self.value)
+
+    def __init__(self, x=[]):
+        self.root = None
+        for e in x:
+            self.insert(e)
+
+    def rotate_right(self, node):
+        """right rotation...
+        https://en.wikipedia.org/wiki/AVL_tree#Simple_rotation
+        """
+        tmp = self.Node(node.value)
+        tmp.priority = node.priority
+        tmp.left, tmp.right = node.left.right, node.right
+        # rotate...
+        node.value, node.priority = node.left.value, node.left.priority
+        node.left, node.right = node.left.left, tmp
+
+    def rotate_left(self, node):
+        """left rotation...
+        https://en.wikipedia.org/wiki/AVL_tree#Simple_rotation
+        """
+        tmp = self.Node(node.value)
+        tmp.priority = node.priority
+        tmp.left, tmp.right = node.left, node.right.left
+        # rotate...
+        node.value, node.priority = node.right.value, node.right.priority
+        node.left, node.right = tmp, node.right.right
+
+    def insert(self, value):
+        """insert a new node with the given value in the tree
+        standard bst insertion + rotations to keep the heap invariance
+        """
+        if value == None: return
+        if not self.root: self.root = self.Node(value); return
+        # execute standard bst insertion...
+        def r(node):
+            if value == node.value: raise ValueError(f'No duplicates allowed...')
+            if value < node.value:
+                if node.left: r(node.left)
+                else: node.left = self.Node(value)
+            else:
+                if node.right: r(node.right)
+                else: node.right = self.Node(value)
+            # repair max_heap invariants...
+            if value > node.value:
+                if node.right.priority > node.priority:
+                    self.rotate_left(node)
+            else:
+                if node.left.priority > node.priority:
+                    self.rotate_right(parent)
+        r(self.root)
+
+    def delete(self, value):
+        """search and delete the node with the given value in the tree
+        rotate the node to be deleted with the max child until if becomes a leaf
+        then just erase the leaf...
+        """
+        if value == None: return
+        # standard binary search for the node, and the parent
+        if not self.root: raise ValueError(f'{value} not found')
+        def get(node, parent):
+            if not node: raise ValueError(f'{value} not found')
+            if value == node.value: return (node, parent)
+            if value < node.value: return get(node.left, node)
+            else: return get(node.right, node)
+        node, parent = get(self.root, None)
+        # make it a leaf so we can get rid of it easily
+        def r(node, parent):
+            # base case: node is a leaf
+            if not node.left and not node.right:
+                # are you root?
+                if not parent: self.root = None; return
+                if parent.right == node: parent.right = None; return
+                else: parent.left = None; return
+            # rotate with max_child, until no childs remain...
+            priority = lambda node: node.priority if node else -float('inf')
+            max_child = max(node.left, node.right, key=lambda node: priority(node))
+            if max_child == node.right:
+                self.rotate_left(node)
+                return r(node.left, node)
+            else:
+                self.rotate_right(node)
+                return r(node.right, node)
+        return r(node, parent)
+    
+    def search(self, value):
+        """standard binary search in the tree
+        """
+        if not self.root: raise ValueError(f'{value} not found')
+        def r(node):
+            if not node: raise ValueError(f'{value} not found')
+            if value == node.value: return node
+            if value < node.value: return r(node.left)
+            else: return r(node.right)
+        return r(self.root)
+
+    def __repr__(self):
+        if not self.root: return
+        res = []
+        def r(node, level=0):
+            if not node: return
+            r(node.left, level +1)
+            res.append('\t'*level + f'-->({node.value}){node.priority:0.04}')
+            r(node.right, level +1)
+        r(self.root)
+        return '\n'.join(res)
+
+# local test...
+# from random import uniform
+# test = list(set([int(uniform(1, 100)) for _ in range(80)]))
+# test = [2, 3, 7, 9, 10, 11, 13, 17, 18]
+test = [1, 2, 3, 11, 12, 14, 18, 19, 22, 29, 31, 33, 38, 41, 44, 51, 55, 57, 62, 63, 66, 70, 72, 73, 76, 81, 88, 90, 93, 97, 98]
+print(f'testing with: {test}')
+treap = Treap(test)
+print(treap)

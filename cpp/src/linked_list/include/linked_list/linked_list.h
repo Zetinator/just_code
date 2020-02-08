286cc9a282cc87ce2523eec7cc2079cd432cd5de
Zetinator <erick_zetina@hotmail.com>
2020-02-07 18:57:52 -0600

    it doesnt compile!

--- cpp/src/linked_list/include/linked_list/linked_list.h
+++ cpp/src/linked_list/include/linked_list/linked_list.h
@@ -4,29 +4,18 @@ namespace linked_list
 {
 
 template<typename T>
-class Node {
-  private:
-    T value;
-    Node next;
-  public:
-    Node(T value);
-    ~Node();
-    // print operator
-    operator std::string();
-};
-    
-template<typename T>
 class LinkedList {
-  private:
-    Node<T> head;
-  public:
-    LinkedList(std::vector<T> data);
-    ~LinkedList();
-    // print operator
-    operator std::string();
-    Node<T> insert();
-    Node<T> search();
-    void erase();
+	private:
+		class Node;
+		Node<T> head;
+	public:
+		LinkedList(std::vector<T> data);
+		~LinkedList();
+		// print operator
+		operator std::string();
+		Node<T> insert();
+		Node<T> search();
+		void erase();
 };
 
 } // namespace Node
--- cpp/src/linked_list/src/linked_list.cpp
+++ cpp/src/linked_list/src/linked_list.cpp
@@ -2,15 +2,24 @@
 
 namespace linked_list
 {
-template<typename T>
-Node<T>::Node(T v) {
-    T value(v);
-    Node<T>* next(nullptr_t);
-}
 
 template<typename T>
-LinkedList<T>::LinkedList(std::vector<T> data) {
-    Node<T>* head(nullptr_t);
+class LinkedList<T>::Node {
+	//basic chainable unit
+	private:
+		T value;
+		Node* next;
+	public:
+		Node(const T& _value): value(_value), next(nullptr_t){};
+		~Node();
+		// print operator
+		operator std::string(){return std::string(value);};
+};
+
+template<typename T>
+//LinkedList<T>::LinkedList(std::vector<T> data) {
+LinkedList<T>::LinkedList(uint32_t _value) {
+	Node* head(_value);
 }
 
 } // namespace Node
@@ -18,12 +27,15 @@ LinkedList<T>::LinkedList(std::vector<T> data) {
 // only here for testing
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << "1.0.0" << ".\n";
-    std::cout << "Usage: " << argv[0] << " <your name>" << std::endl;
-    return 1;
-  }
-    std::cout << argv[1] << " quiere mucho a su marion :(" << std::endl;
-    return 0;
+	if (argc < 2) {
+		// report version
+		std::cout << argv[0] << " Version " << "1.0.0" << ".\n";
+		std::cout << "Usage: " << argv[0] << " <your name>" << std::endl;
+		return 1;
+	}
+
+	//std::vector<uint32_t> test = {1, 2, 3, 4, 5};
+	auto node = linked_list::LinkedList<uint32_t>(69);
+	std::cout << argv[1] << " quiere mucho a su marion :(" << std::endl;
+	return 0;
 }
